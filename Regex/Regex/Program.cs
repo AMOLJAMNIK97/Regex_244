5743e9b88e1f3c8b5161654a0ea84d5138c9c1f3
Amol <ajamnik.mech@gmail.com>
2023-01-25 00:05:39 +0530

    [Amol]- Check First Name is Vaild or Not

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            RegularExpressions regular = new RegularExpressions();
+            regular.FirstName();
+        }
+    }
+}
---
+++ Regex/Regex/RegularExpressions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    public class RegularExpressions
+    {
+        
+        Regex regex = new Regex("^[A-Z]{1}[a-z]{3}$");
+        public void FirstName()
+        {
+            Console.WriteLine("Enter First Name");
+            var data = Console.ReadLine();
+
+            if (regex.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(data + " is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(data + " is Not Valid");
+                Console.ResetColor();
+            }
+        }
+    }
+}
